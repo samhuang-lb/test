334de981ed8755c9fca2a6640da2c38db5f26e2a
samhuang-lb <sam.huang@lifebyte.io>
2025-04-30 18:15:09 +0800

    a2

--- a.ts
+++ a.ts
@@ -2,3 +2,8 @@ type Concat<T extends any[], U extends any[]> = [...T, ...U];
 type Tuple1 = [1, 2];
 type Tuple2 = [3, 4];
 type MergedTuple = Concat<Tuple1, Tuple2>; //  [1, 2, 3, 4]
+type OOO={
+    a?:number
+    b:string
+}
+type xx=NonNullable<OOO['a']>
