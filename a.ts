dce62799e2dd8a758a7f0c7da1c5c0fd19ea746f
samhuang-lb <sam.huang@lifebyte.io>
2025-05-09 17:46:43 +0800

    a3

--- a.ts
+++ a.ts
@@ -7,3 +7,29 @@ type OOO={
     b:string
 }
 type xx=NonNullable<OOO['a']>
+
+type DeepPartial<T> = {
+    [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
+};
+
+// 示例
+interface User {
+    id: number;
+    name: string;
+    address: {
+        city: string;
+        zip: number;
+    };
+    hobbies: string[];
+};
+
+// 使用 DeepPartial
+type PartialUser = DeepPartial<User>;
+
+// 测试
+const user4: PartialUser = {
+    id: 1,
+    address: {
+        zip: 10001, // 只提供嵌套对象的部分属性
+    },
+};
